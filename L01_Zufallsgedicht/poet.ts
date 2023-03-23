38604988514873927d76975d9cb54691d2eba569
MareikeMKB <mareike.bitz@gmx.net>
2023-03-23 13:48:44 +0100

    funktioniert

--- L01_Zufallsgedicht/poet.ts
+++ L01_Zufallsgedicht/poet.ts
@@ -1,18 +1,18 @@
 namespace L01_Zufallsgedicht {
-    let subjekte = ['Bellatrix', 'Andromeda', 'Narcissa', 'Sirius', 'Regulus', 'Kreacher']
-    let adjektive = ['foltert','liebt','bewundert','hasst','zerstört','freut sich auf']
-    let objekte = ['Ted Tonks', 'den Dunklen Lord', 'Lucius', 'Remus', 'das Amulett Slytherins', 'Zaubersprüche']
-    console.log(subjekte, adjektive, objekte);
+    let subjekte = ['Bellatrix', 'Andromeda', 'Narcissa', 'Sirius', 'Regulus', 'Kreacher'];
+    let adjektive = ['foltert','liebt','bewundert','hasst','zerstört','freut sich auf'];
+    let objekte = ['Ted Tonks', 'den Dunklen Lord', 'Lucius', 'Remus', 'das Amulett Slytherins', 'Hogwarts'];
 
     for (let i = 6; i > 0; i--) {
-        console.log(i);
-        getVerse(subjekte[0], adjektive[1], objekte[1]);
+        let x:number = Math.floor(Math.random()*subjekte.length);
+        let y:number = Math.floor(Math.random()*subjekte.length);
+        let z:number = Math.floor(Math.random()*subjekte.length);
+        getVerse(subjekte.splice(x,1)[0], adjektive.splice(y,1)[0], objekte.splice(z,1)[0]);
     }
 
     function getVerse (_string1:string, _string2:string, _string3:string) {
         let verse = _string1 + ' ' + _string2 + ' ' + _string3
-        console.log(verse)
-        console.log('Accio!')
+        console.log(verse);
     }
 
 
