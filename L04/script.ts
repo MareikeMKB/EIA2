909378631c8bdf2b9191aa9394590c0a8015776e
MareikeMKB <mareike.bitz@gmx.net>
2023-04-22 23:13:24 +0200

    vorrübergehend

---
+++ L04/script.ts
@@ -0,0 +1,13 @@
+namespace Datensammlung {
+    console.log('Hallo')
+    /*
+      Aufgabe: <Aufgabe 4 ToDo>
+      Name: <Mareike Bitz>
+      Matrikel: <272506>
+      Datum: <22.04.23>
+      Quellen: <>
+      */
+
+
+
+}
