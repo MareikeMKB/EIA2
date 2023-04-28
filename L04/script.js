0a17f6c8a2c457a95788ac5b5f0edc28d6f81682
MareikeMKB <mareike.bitz@gmx.net>
2023-04-28 19:06:37 +0200

    vielleicht endlich geschafft?

--- L04/script.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var L03_Aufgabenliste_Formular;
-(function (L03_Aufgabenliste_Formular) {
-    console.log('Hallo');
-})(L03_Aufgabenliste_Formular || (L03_Aufgabenliste_Formular = {}));
-//# sourceMappingURL=script.js.map
---
+++ L04_Aufgabenliste_Datenstruktur/script.ts
@@ -0,0 +1,72 @@
+namespace Datensammlung {
+    console.log('Hallo')
+    /*
+      Aufgabe: <Aufgabe 4 ToDo>
+      Name: <Mareike Bitz>
+      Matrikel: <272506>
+      Datum: <28.04.23>
+      Quellen: <Judith Pauler>
+      */
+
+    let taskArray1: String[] = [];
+
+    function getData(): String[] {
+  
+        let form: HTMLFormElement = document.querySelector('#form1')!;
+        let taskArray: String[];
+
+        let formData = new FormData(form);
+        console.log(formData);
+        let p0 = formData.get('taskname') as string;
+        let p1 = formData.get('date') as string;
+        let p2 = formData.get('comment') as string;
+        let p3 = formData.get('Person') as string;
+        let p4 = formData.get('box') as string; 
+
+        taskArray = [p0, p1, p2, p3, p4];
+        console.log("getData: " + taskArray);
+        taskArray1 = taskArray; 
+        return taskArray1;
+    }; 
+  
+    let wrap = <HTMLElement>document.querySelector("#wrapper");
+
+    document.querySelector("#add")!.addEventListener("click", function () {
+        wrap.style.setProperty("visibility", "visible");
+
+    });
+    
+    document.querySelector("#add2")!.addEventListener("click", function (f) {
+      wrap.style.setProperty("visibility", "hidden"); 
+      getData();
+      let newdiv = document.createElement("div");
+      newdiv.setAttribute("id", "newtask");
+      let newP = document.createElement("p"); 
+      newdiv.setAttribute("id", "newp");
+      document.getElementById("div1")!.appendChild(newdiv);
+      document.querySelector("#div1")!.appendChild(newP);
+      newP.innerHTML = "Task: "+ taskArray1[0] + " | to finish until the "+ taskArray1[1]+  " | Worked on by "+ taskArray1[3] + "  || Comment: "+ taskArray1[2] + "     ";
+      f.preventDefault();
+
+      let Delete = document.createElement("button");
+      Delete.setAttribute("id", "trash");
+      newP.appendChild(Delete);
+      Delete.innerHTML = "Delete";
+
+      Delete.addEventListener("click", function () {
+        this!.parentNode!.parentNode!.removeChild(this!.parentNode!);
+    });
+    
+    let edit = document.createElement("button");
+    edit.setAttribute("id", "edit");
+    newP.appendChild(edit);
+    edit.innerHTML = "Edit";
+
+    edit.addEventListener("click", function () {
+      wrap.style.setProperty("visibility", "visible");
+    });
+    });
+  
+  }
+
+   
