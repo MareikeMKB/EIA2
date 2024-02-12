eef89a2d5e1e450b659d0b7a11803f98f03fabb9
MareikeMKB <mareike.bitz@gmx.net>
2024-02-12 18:17:00 +0100

    edit

--- EIA2-2_Website/script.js
+++ EIA2-2_Website/script.js
@@ -4,9 +4,11 @@ var Website;
     console.log('Hallo');
     let answers = ['#a1', '#a2', '#a3', '#a4'];
     let codes = ['Code1', 'Code2', 'Code3', 'Code4'];
+    document.getElementById("stuff").style.setProperty("visibility", "hidden");
     for (let i = 0; i < 4; i++) {
         document.querySelector(answers[i])?.addEventListener("click", function () {
             document.getElementById("p").innerHTML = codes[i];
+            document.getElementById("stuff").style.setProperty("visibility", "visible");
         });
     }
 })(Website || (Website = {}));
